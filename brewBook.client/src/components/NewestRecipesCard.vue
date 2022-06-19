134f0b6811b6279fe9f4ef579ccaee9bb60fa5fc
JacksonHagen <jackson.d.hagen@gmail.com>
2022-06-19 00:00:26 -0600

    favicon added and newest recipes started

---
+++ brewBook.client/src/components/NewestRecipesCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card text-secondary bg-warning w-100">
+    <div class="card-header">Newest Filter Recipes</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
--- brewBook.client/src/pages/HomePage.vue
+++ brewBook.client/src/pages/HomePage.vue
@@ -1,4 +1,12 @@
-<template></template>
+<template>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-md-6">
+        <NewestRecipesCard />
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
